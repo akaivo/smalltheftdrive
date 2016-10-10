66dcdc1fac77175c34fd07d4fa6828907bb8315f
Aadam Kaivo <akaivo@ut.ee>
2016-10-10 11:41:36 +0300

    what

--- Assets/scripts/BulletMaker.cs
+++ Assets/scripts/BulletMaker.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class BulletMaker : MonoBehaviour {
 	public GameObject bulletPrefab;
-	public float bulletSpeed;
 	// Use this for initialization
 	void Start () {
 	
