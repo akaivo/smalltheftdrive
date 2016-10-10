e2d5e69a355c7d12ac6a8caad50e406053154e7b
Aadam Kaivo <akaivo@ut.ee>
2016-10-10 14:54:36 +0300

    random bunnies

---
+++ Assets/scripts/Bunny.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bunny : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(Random.Range(-5f,5f),10,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += Vector3.down * GameMain.speed * Time.deltaTime;
+		if (transform.position.y < -10) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/scripts/GameMain.cs
+++ Assets/scripts/GameMain.cs
@@ -4,8 +4,19 @@ using System.Collections;
 public class GameMain : MonoBehaviour {
 	public static float speed;
 	public float setSpeed;
+	public GameObject bunnyPrefab;
+	public float bunninity;
+
 	// Use this for initialization
 	void Start () {
 		speed = setSpeed;
+	}
+
+	void Update(){
+		float number = Random.value;
+		float chanceOfBunny = Time.deltaTime* bunninity;
+		if (chanceOfBunny > number) {
+			GameObject bunnyGo = Instantiate (bunnyPrefab);
+		}
 	}
 }
