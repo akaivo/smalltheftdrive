34f465235919e3acffd187c4f14189106918681c
Aadam Kaivo <akaivo@ut.ee>
2016-10-10 14:21:01 +0300

    bullet destroy

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 	public float bulletSpeed;
 
-
+	void Update(){
+		if (transform.position.magnitude > 16) {
+			Destroy (gameObject);
+		}
+	}
     void OnCollisionEnter()
     {
         Destroy(gameObject);
