ed20b88c7a5efa2a9d60d9a675d6afa5df46f659
Aadam Kaivo <akaivo@ut.ee>
2016-10-13 20:44:02 +0300

    stars and rainbowss

---
+++ Assets/scripts/Rainbow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rainbow : MonoBehaviour {
+	public GameObject starPrefab;
+	// Use this for initialization
+	void Start () {
+		makeStars (Random.Range (3, 5));
+	}
+
+	void makeStars(int stars){
+		for (int i = 0; i < stars; i++) {
+			int angle = Random.Range (0, 360);
+			Vector3 direction = Quaternion.AngleAxis (angle, Vector3.forward) * Vector3.right;
+			direction.z = 0;
+			direction.Normalize ();
+
+			GameObject star = Instantiate (starPrefab);
+			star.transform.parent = gameObject.transform;
+			star.transform.position = gameObject.transform.position;
+			Rigidbody2D rb = star.GetComponent<Rigidbody2D> ();
+			rb.velocity = direction * Random.Range (0.3f, 4f);
+		}
+	}
+	
+}
---
+++ Assets/scripts/Star.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+	public float start;
+	public float rate;
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(start, start, start);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += ((new Vector3 (rate,rate,rate)) *Time.deltaTime);
+	}
+}
