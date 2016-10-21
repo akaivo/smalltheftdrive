be6873605e340b2f65cf2cf4409ab67e3156438f
Aadam Kaivo <akaivo@ut.ee>
2016-10-21 12:28:11 +0300

    scoring

--- Assets/scripts/Bunny.cs
+++ Assets/scripts/Bunny.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Bunny : MonoBehaviour {
 	public GameObject rainbowPrefab;
+	public int points;
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector3(Random.Range(-3.5f,3.5f),10,0);
@@ -17,7 +18,9 @@ public class Bunny : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
+		ScoreKeeper.score += points;
 		GameObject rainbow = Instantiate (rainbowPrefab);
 		rainbow.transform.position = transform.position;
 	}
+		
 }
---
+++ Assets/scripts/ScoreKeeper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+	public static int score = 0;
+	Text scoreText;
+	// Use this for initialization
+	void Start () {
+		scoreText = GetComponent<Text> ();
+		scoreText.text = "$: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "$: " + score;
+	}
+}
